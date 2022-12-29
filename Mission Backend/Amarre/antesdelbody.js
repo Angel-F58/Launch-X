9e75b0f34efc33d79803a241cd37093ed7766c28
Angel-F58 <114125783+Angel-F58@users.noreply.github.com>
2022-12-28 22:46:04 -0600

    Archivos y carpeta multimedia
    Se subieron los archivos HTML, CSS y JS de la práctica y una carpeta con un audio MP3 y un video MP4 que se utilizaron en la página web

---
+++ Mission Backend/Amarre/antesdelbody.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    //Me crea una serie de elements por cada tag encontrado
+    let encabezados = document.createElement("h1");
+    const texto = document.createTextNode("¿Quieres ser mi chava?");
+    encabezados.appendChild(text);
+    document.body.appendChild(encabezados);
+
+    console.log(encabezados);
+    console.log(texto);
+}
---
+++ Mission Backend/Amarre/app.js
@@ -0,0 +1,28 @@
+function moverBotonNO(elm) {
+    elm.style.position = 'absolute';
+    elm.style.top = Math.random() * (window.innerHeight - elm.offsetHeight) + 'px';
+    elm.style.left = Math.random() * (window.innerWidth - elm.offsetWidth) + 'px';
+}
+
+let btnSi = document.getElementById("btn_si");
+let btnNo = document.getElementById("btn_no");
+let divModoSexo = document.getElementsByClassName("modo_sexo")[0];
+
+btnNo.addEventListener('mouseenter', function(e) { moverBotonNO(e.target) });
+
+btnSi.addEventListener('click', function(e) {
+    alert('Sabía que dirías que sí. CASEMONOS YA. TE AMO');
+    divModoSexo.style.display = 'block';
+    const cancion = new Audio('images/img_modo_hot.mp3');
+    cancion.play();
+})
+
+
+
+botones = document.getElementsByTagName("button");
+console.log(botones);
+
+window.addEventListener('beforeunload', (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+})
